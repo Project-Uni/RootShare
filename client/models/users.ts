83dfe0f929ebb7c3c2ccd1764fcf8c7797fffbad
Smit Desai <smitdesai422@gmail.com>
2020-05-11 12:14:30 -0500

    Update User Schema
    Made 'email' field in User schema unique.

--- client/models/users.ts
+++ client/models/users.ts
@@ -4,7 +4,7 @@ var Schema = mongoose.Schema
 var userSchema = new Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
-    email: {type: String, required: true},
+    email: {type: String, unique: true, required: true},
     hashedPassword: {type: String, required: true},
     university: {type: String, required: true},
     accountType: {type: String, required: true},
