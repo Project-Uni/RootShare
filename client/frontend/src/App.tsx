7a66e7c6034bf4b3f3b6d9ddac971f27d054f589
Ashwin <mahesh.ashwin1998@gmail.com>
2020-05-18 02:11:55 -0400

    Setup files for hype pages

--- client/frontend/src/App.tsx
+++ client/frontend/src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+import HypeLanding from "./hype-page/hype-landing/HypeLanding";
+import HypeRegistration from "./hype-page/hype-registration/HypeRegistration";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <div className="wrapper">
+          <Switch>
+            <Route exact path="/">
+              <HypeLanding />
+            </Route>
+            <Route exact path="/register">
+              <HypeRegistration />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
