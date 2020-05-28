e705c3f7bf802bf2f9da7da052da8b683b7dd73e
Ashwin <mahesh.ashwin1998@gmail.com>
2020-05-27 21:49:42 -0700

    Before switching to new layout for hype page

--- client/frontend/src/hype-page/hype-landing/HypeLanding.tsx
+++ client/frontend/src/hype-page/hype-landing/HypeLanding.tsx
@@ -1,37 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import HypeBackground from "../../images/CreatedHypeBG.png";
 import { AppBar, Typography, Button, Toolbar } from "@material-ui/core";
+// import HypeBackground from "../../images/CreatedHypeBG.png";
+import HypeBackground from "../../images/PurdueHypeAlt.png";
+import RootShareLogo from "../../images/RootShareLogoWhite.png";
 
 const useStyles = makeStyles((_: any) => ({
-  wrapper: {
-    // minWidth: "600px",
-  },
+  wrapper: {},
   background: {
     width: "100vw",
     backgroundImage: `url(${HypeBackground})`,
-    // height: "auto",
-    height: "1600px",
+    height: `51.34vw`,
     backgroundRepeat: "no-repeat",
     backgroundSize: "100vw auto",
-    // background: "radial-gradient(closest-side, white, black)",
   },
   registerLink: {
-    // background: "linear-gradient(45deg, rgb(0, 85, 255), rgb(84, 167, 255))",
     background: "linear-gradient(45deg, rgb(20, 29, 156), rgb(73, 106, 252))",
     borderRadius: 3,
     border: 0,
     color: "white",
-    // height: 48,
     fontSize: "18pt",
     padding: "10px 60px",
     boxShadow: "0 3px 5px 2px rgba(13, 89, 255, .3)",
-    marginTop: "500px",
+    // marginTop: "500px",
+    marginTop: "30vw",
   },
   interior: {
     background: "radial-gradient(closest-side, rgba(0,0,0,0), rgba(0,0,0,0.1))",
     width: "100%",
-    height: "700px",
+    height: `51.34vw`,
   },
   headerTitle: {
     flexGrow: 1,
@@ -47,32 +44,45 @@ const useStyles = makeStyles((_: any) => ({
   buttonLink: {
     textDecoration: "none",
   },
+  headerLogo: {
+    color: "white",
+    height: "38px",
+    width: "190px",
+  },
 }));
 
 type Props = {};
 
 function HypeLanding(props: Props) {
+  const [width, setWidth] = useState(0);
   const styles = useStyles();
+
   return (
     <div className={styles.wrapper}>
       <AppBar position="static" className={styles.header}>
         <Toolbar>
-          <Typography className={styles.headerTitle} variant="h5">
+          {/* <Typography className={styles.headerTitle} variant="h5">
             RootShare
-          </Typography>
+          </Typography> */}
+          <div className={styles.headerTitle}>
+            <img
+              src={RootShareLogo}
+              alt="RootShare"
+              className={styles.headerLogo}
+            />
+          </div>
+
           <a href="/register" className={styles.buttonLink}>
             <Button className={styles.headerRegisterButton}>Register</Button>
           </a>
         </Toolbar>
       </AppBar>
       <div className={styles.background}>
-        <div className={styles.interior}>
-          <a href="/register" className={styles.buttonLink}>
-            <Button variant="outlined" className={styles.registerLink}>
-              Join Now
-            </Button>
-          </a>
-        </div>
+        <a href="/register" className={styles.buttonLink}>
+          <Button variant="outlined" className={styles.registerLink}>
+            Join Now
+          </Button>
+        </a>
       </div>
     </div>
   );
