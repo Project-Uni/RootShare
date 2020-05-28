b5a8d2ff974df27a15c25ece22d827e6d0c85332
Smit Desai <smitdesai422@gmail.com>
2020-05-28 12:47:11 -0500

    Redirect users to unconfirmed page if they try to access confirmed endpoint

--- client/routes/middleware/isConfirmed.ts
+++ client/routes/middleware/isConfirmed.ts
@@ -1,9 +1,7 @@
 module.exports = function (req, res, next) {
-  // If the user is logged in, continue with the request to the restricted route
   if (req.user.confirmed) {
     return next();
   }
-  // Send response to the frontend telling them they aren't authenticated
-  // and display the login page to them
-  return res.json('NOT CONFIRMED');
+
+  return res.redirect('secure-unconfirmed');
 };
