73c5c8cac8e31e9e3f5bd883e3cd848ce03f5f38
Smit Desai <smitdesai422@gmail.com>
2020-06-09 15:24:49 -0500

    Create opentok sessions and generate tokens

--- client/interactions/opentok/sessions.ts
+++ client/interactions/opentok/sessions.ts
@@ -1,20 +1,42 @@
 const OpenTok = require('opentok')
+// const OT = require('@opentok/client')
 const { OPENTOK_API_KEY, OPENTOK_API_SECRET } = require('../../../keys/keys.json')
 const opentok = new OpenTok(OPENTOK_API_KEY, OPENTOK_API_SECRET)
 
 import log from '../../helpers/logger'
+import sendPacket from '../../helpers/sendPacket'
 
 module.exports = {
-  createSession: () => {
-    let sessionId
+  createSession: async () => {
+    let sessionID
 
-    opentok.createSession({ mediaMode: "routed" }, function (error, session) {
+    let session = await opentok.createSession({ mediaMode: "routed" }, function (error, session) {
       if (error) {
         log('error', error)
       } else {
-        sessionId = session.sessionId;
-        log('info', `Session ID: ${sessionId}`);
+        sessionID = session.sessionId;
+        log('info', `Session ID: ${sessionID}`);
       }
     });
+
+    return sessionID
+  },
+
+  getToken: async (sessionID) => {
+    let token = await opentok.generateToken(sessionID, {
+      role: 'publisher',
+      data: 'username=johndoe'
+    })
+
+    return token
+
+    // let session = OT.initSession(OPENTOK_API_KEY, sessionID)
+    // session.connect(token, function (error) {
+    //   if (error) {
+    //     log("error", error.name + error.message);
+    //   } else {
+    //     log('info', 'Connected to the session');
+    //   }
+    // });
   }
 }
--- client/routes/opentok.ts
+++ client/routes/opentok.ts
@@ -1,8 +1,18 @@
 import sendPacket from "../helpers/sendPacket";
 import log from "../helpers/logger";
 
-module.exports = (app) => {
-  app.get('/webinar/createSession', (req, res) => {
+const { createSession, getToken } = require('../interactions/opentok/sessions')
 
+module.exports = (app) => {
+  app.get('/webinar/createSession', async (req, res) => {
+    const sessionID = await createSession()
+    res.json(sessionID)
+  })
+
+  app.post('/webinar/joinSession', async (req, res) => {
+    const { sessionID } = req.body
+    let token = await getToken(sessionID)
+
+    res.json(token)
   })
 }
