336c0288d5435a6873d3ce7a21caca18f43578ec
Ashwin <mahesh.ashwin1998@gmail.com>
2020-06-14 15:17:37 -0700

    Code cleanup

--- client/frontend/src/base-components/RSText.tsx
+++ client/frontend/src/base-components/RSText.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((_: any) => ({
-  wrapper: {},
   title: {
     fontFamily: "Ubuntu",
   },
