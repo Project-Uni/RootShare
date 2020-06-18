1cb2010bffd35422f28150e783b0e9cc3a67b340
Smit Desai <smitdesai422@gmail.com>
2020-06-18 18:02:06 -0500

    Toggle Webcam publishing

--- client/frontend/src/webinar-platform/stream-handling/HostStreamHolder.tsx
+++ client/frontend/src/webinar-platform/stream-handling/HostStreamHolder.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import OT from '@opentok/client'
+import OT, { Session, Publisher } from '@opentok/client'
 import axios from 'axios'
 import { makeStyles } from "@material-ui/core/styles"
 import log from '../../helpers/logger'
@@ -20,21 +20,33 @@ function PublisherStreamHolder(props: Props) {
     }
   }
 
-  function recordScreen() {
+  function recordWebcam() {
     const publisher = OT.initPublisher('publisher', {
       insertMode: 'append',
       width: '10%',
       height: '200px'
     }, handleError);
 
+    console.log("publishing webcam")
+    session.publish(publisher, handleError)
+    return publisher
+  }
+
+  function recordScreen() {
+    const publisher = OT.initPublisher('window', {
+      insertMode: 'append',
+      width: '10%',
+      height: '200px'
+    }, handleError);
+
+    console.log("publishing screen")
+    session.publish(publisher, handleError)
     return publisher
   }
 
   async function connectStream(webinarID: string) {
     if (OT.checkSystemRequirements() !== 1) {
-      // The client does not support WebRTC.
-      // You can display your own message.
-      // log('error', "This device does not support WebRTC")
+      // The client does not support WebRTC
       return
     }
 
@@ -46,7 +58,6 @@ function PublisherStreamHolder(props: Props) {
       }
     });
 
-    const publisher = recordScreen()
     const { data: sessionData } = await axios.post(
       '/webinar/getOpenTokSessionID', {
       webinarID
@@ -66,18 +77,19 @@ function PublisherStreamHolder(props: Props) {
       return
     }
 
-    const session = OT.initSession(OPENTOK_API_KEY, sessionID)
 
-    session.on("streamCreated", function (event: any) {
-      session.subscribe(event.stream);
+    const newSession = OT.initSession(OPENTOK_API_KEY, sessionID)
+
+    newSession.on("streamCreated", function (event: any) {
+      newSession.subscribe(event.stream);
     });
 
-    session.connect(tokenData.content.token, function (error: any) {
+    newSession.connect(tokenData.content.token, function (error: any) {
       if (error) {
         log(error.name, error.message);
       } else {
         log('info', 'Connected to the session.');
-        session.publish(publisher, handleError)
+        setSession(newSession)
       }
     });
   }
@@ -111,6 +123,40 @@ function PublisherStreamHolder(props: Props) {
   }
 
   const [canScreenShare, setCanScreenShare] = useState(false)
+  const [webcamPublisher, setWebcamPublisher] = useState(new Publisher())
+  const [screenPublisher, setScreenPublisher] = useState(new Publisher())
+  const [session, setSession] = useState(new Session())
+
+
+  function toggleWebcam() {
+    setWebcamPublisher((prevState) => {
+      if (session.sessionId === undefined) {
+        return new Publisher()
+      }
+
+      if (prevState.session === undefined) {
+        return recordWebcam()
+      } else {
+        session.unpublish(webcamPublisher)
+        return new Publisher()
+      }
+    })
+  }
+
+  function toggleScreen() {
+    setScreenPublisher((prevState) => {
+      if (session.sessionId === undefined) {
+        return new Publisher()
+      }
+
+      if (prevState.session === undefined) {
+        return recordScreen()
+      } else {
+        session.unpublish(screenPublisher)
+        return new Publisher()
+      }
+    })
+  }
 
   return (
     <div>
@@ -118,8 +164,15 @@ function PublisherStreamHolder(props: Props) {
       <button onClick={createSession}>Create New Session</button>
       <button onClick={startLiveStreaming}>Start Live Streaming</button>
       <button onClick={stopLiveStreaming}>Stop Live Streaming</button>
-      {canScreenShare ? <button>Share Screen</button> : <div></div>}
-    </div>
+
+      {session.sessionId !== undefined ?
+        <button onClick={toggleWebcam}>Toggle Webcam</button> :
+        <div></div>}
+
+      {(canScreenShare && session.sessionId !== undefined) ?
+        <button onClick={toggleScreen}>Toggle Screen</button> :
+        <div></div>}
+    </div >
   )
 }
 
