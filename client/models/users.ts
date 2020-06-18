ac77f750e400ef961ecbfef7c21d8f49f4319c43
Ashwin <mahesh.ashwin1998@gmail.com>
2020-06-17 19:16:31 -0700

    Add timestamps to user

--- client/models/users.ts
+++ client/models/users.ts
@@ -1,28 +1,31 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  university: { type: Schema.ObjectId, ref: "universities" },
-  accountType: { type: String },
-  hashedPassword: String,
-  googleID: String,
-  linkedinID: String,
-  graduationYear: Number,
-  department: String,
-  major: String,
-  phoneNumber: String,
-  organizations: Array,
-  work: String,
-  position: String,
-  interests: Array,
-  graduateSchool: String,
-  discoveryMethod: String,
-  sendEmails: { type: Boolean, required: true, default: true },
-  confirmed: { type: Boolean, required: true, default: false },
-  verified: { type: Boolean, required: true, default: false },
-});
+var userSchema = new Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    university: { type: Schema.ObjectId, ref: "universities" },
+    accountType: { type: String },
+    hashedPassword: String,
+    googleID: String,
+    linkedinID: String,
+    graduationYear: Number,
+    department: String,
+    major: String,
+    phoneNumber: String,
+    organizations: Array,
+    work: String,
+    position: String,
+    interests: Array,
+    graduateSchool: String,
+    discoveryMethod: String,
+    sendEmails: { type: Boolean, required: true, default: true },
+    confirmed: { type: Boolean, required: true, default: false },
+    verified: { type: Boolean, required: true, default: false },
+  },
+  { timestamps: true }
+);
 
 mongoose.model("users", userSchema);
