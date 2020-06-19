c7fe2d1f425ac41a0e0fd6e856bfeec44d9b728a
Ashwin <mahesh.ashwin1998@gmail.com>
2020-06-18 17:23:43 -0700

    Added joined today to count page

--- client/frontend/src/admin-utility/UserCount.tsx
+++ client/frontend/src/admin-utility/UserCount.tsx
@@ -3,12 +3,20 @@ import { makeStyles } from "@material-ui/core/styles";
 
 import axios from "axios";
 
+import RootShareLogoFull from "../images/RootShareLogoFull.png";
+
+import HypeHeader from "../hype-page/headerFooter/HypeHeader";
 import RSText from "../base-components/RSText";
 
 const useStyles = makeStyles((_: any) => ({
   wrapper: {},
   textStyle: {
     textAlign: "center",
+  },
+  rootshareLogo: {
+    height: "80px",
+    marginLeft: 10,
+    marginBottom: 20,
   },
 }));
 
@@ -17,6 +25,7 @@ type Props = {};
 function UserCount(props: Props) {
   const styles = useStyles();
   const [users, setUsers] = useState([]);
+  const [joinedToday, setJoinedToday] = useState(0);
 
   useEffect(() => {
     const password = prompt("Enter password:");
@@ -31,11 +40,36 @@ function UserCount(props: Props) {
     const { data } = await axios.get("/api/adminCount");
     if (data.success === 1) {
       setUsers(data["content"]["users"]);
+      calculateJoinedToday(data["content"]["users"]);
     }
+  }
+
+  function calculateJoinedToday(users: [any]) {
+    let newUserCount = 0;
+    const currentDate = new Date();
+
+    const day = currentDate.getDate();
+    const month = currentDate.getMonth();
+    const year = currentDate.getFullYear();
+
+    const morning = new Date(year, month, day, 5, 0);
+
+    for (let i = 0; i < users.length; i++) {
+      if ("createdAt" in users[i]) {
+        const difference = +new Date(users[i]["createdAt"]) - +morning;
+        const hourDiff = Math.floor(difference / (1000 * 60 * 60));
+
+        if (hourDiff > 0) {
+          newUserCount += 1;
+        }
+      }
+    }
+    setJoinedToday(newUserCount);
   }
 
   function renderUsers() {
     const output = [];
+
     for (let i = 0; i < users.length; i++) {
       output.push(
         <div style={{ marginTop: 15 }}>
@@ -50,9 +84,21 @@ function UserCount(props: Props) {
 
   return (
     <div className={styles.wrapper}>
+      <HypeHeader />
+      <img
+        src={RootShareLogoFull}
+        className={styles.rootshareLogo}
+        alt="RootShare"
+      />
       <RSText type="head" className={styles.textStyle} size={32}>
-        {users.length}
+        {users.length} Users
       </RSText>
+      <div style={{ marginTop: 20 }}>
+        <RSText type="head" className={styles.textStyle} size={24}>
+          {joinedToday} joined today
+        </RSText>
+      </div>
+
       <div style={{ marginTop: 30 }}>{renderUsers()}</div>
     </div>
   );
