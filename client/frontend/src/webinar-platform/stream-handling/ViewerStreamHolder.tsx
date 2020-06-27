6bbe86e54fd910b75d617de3ec51cae42482835d
Ashwin Mahesh <ashwin@Ashwins-MBP-2.attlocal.net>
2020-06-27 11:22:00 -0700

    Made Viewer more reacty

--- client/frontend/src/webinar-platform/stream-handling/ViewerStreamHolder.tsx
+++ client/frontend/src/webinar-platform/stream-handling/ViewerStreamHolder.tsx
@@ -1,58 +1,60 @@
-import React, { useState, useEffect } from "react"
-import { makeStyles } from "@material-ui/core/styles"
-import axios from 'axios'
+import React, { useState, useEffect } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import axios from 'axios';
 
-import VideoPlayer from './VideoPlayer'
+import VideoPlayer from './VideoPlayer';
 
-import log from '../../helpers/logger'
+import log from '../../helpers/logger';
 
 const useStyles = makeStyles((_: any) => ({
   wrapper: {},
 }));
 
-type Props = {}
+type Props = {};
 
 function ViewerStreamHolder(props: Props) {
-  const styles = useStyles()
-  const [videoData, setVideoData] = useState('')
+  const styles = useStyles();
+  const [videoData, setVideoData] = useState('');
 
   useEffect(() => {
-    setSourceToLatestWebinarID()
-  }, [])
+    setSourceToLatestWebinarID();
+  }, []);
 
   async function setSourceToLatestWebinarID() {
-    const webinarID = await axios.get('/webinar/latestWebinarID')
-      .then((response) => {
-        return response.data.content.webinarID
-      }).catch((err) => {
-        log('error', err)
-      })
+    const { data } = await axios.get('/webinar/latestWebinarID');
+    let webinarID = '';
+    if (data['success'] === 1) {
+      webinarID = data.content['webinarID'];
+    }
+    else {
+      log('error', data['message']);
+      return;
+    }
+    getVideoData(webinarID);
+  }
 
-    axios.post('/webinar/getMuxPlaybackID', { webinarID })
-      .then(async (response) => {
-        const { success, message } = response.data
-        if (success === 1) {
-          const { muxPlaybackID } = response.data.content
-          const source = `https://stream.mux.com/${muxPlaybackID}.m3u8`
-          const streamExists = await checkStreamExists(source)
-          if (streamExists) {
-            setVideoData(source)
-          }
-        } else {
-          log('error', message)
-        }
-      }).catch((err) => {
-        log('error', err)
-      })
+  async function getVideoData(webinarID: string) {
+    const { data } = await axios.post('/webinar/getMuxPlaybackID', { webinarID });
+    if (data['success'] === 1) {
+      const { muxPlaybackID } = data.content;
+      const source = `https://stream.mux.com/${muxPlaybackID}.m3u8`;
+      const streamExists = await checkStreamExists(source);
+
+      if (streamExists) setVideoData(source);
+      else log('error', data['message']);
+    }
+    else {
+      log('error', data['message']);
+    }
   }
 
   async function checkStreamExists(source: string) {
     return await axios.get(source)
       .then((response) => {
-        return true
+        return true;
       }).catch((err) => {
-        return false
-      })
+        return false;
+      });
   }
 
 
@@ -60,7 +62,7 @@ function ViewerStreamHolder(props: Props) {
     <div className={styles.wrapper}>
       {videoData !== '' ? <VideoPlayer src={videoData} /> : <></>}
     </div>
-  )
+  );
 }
 
-export default ViewerStreamHolder
+export default ViewerStreamHolder;
