ce41c57121b9776b1b0b9b8f7298bc8e959ac19f
Smit Desai <smitdesai422@gmail.com>
2020-07-08 11:09:04 -0700

    Moved API Key into env variable

--- client/frontend/src/event-client/event-video/event-host/helpers/functions/connectStreamHelpers.ts
+++ client/frontend/src/event-client/event-video/event-host/helpers/functions/connectStreamHelpers.ts
@@ -2,8 +2,6 @@ import axios from 'axios';
 import OT from '@opentok/client';
 import log from '../../../../../helpers/logger';
 import { createNewWebcamPublisher } from './createPublishers';
-//Ashwin - We should be storing this on the frontend I believe, I might be wrong. Not a good idea to pass it from outside of the frontend repo
-const { OPENTOK_API_KEY } = require('../../../../../keys.json');
 
 export async function validateSession(webinarID: string) {
   const { data } = await axios.post('/webinar/getOpenTokSessionID', {
@@ -43,7 +41,10 @@ export async function createEventSession(
   ) => void,
   setCameraPublisher: (newPublisher: OT.Publisher) => void
 ) {
-  const eventSession = OT.initSession(OPENTOK_API_KEY, sessionID);
+  let eventSession: OT.Session;
+  if (process.env.REACT_APP_OPENTOK_API_KEY) {
+    eventSession = OT.initSession(process.env.REACT_APP_OPENTOK_API_KEY, sessionID);
+  } else return;
   addEventSessionListeners(eventSession, updateVideoElements, removeVideoElement);
 
   await eventSession.connect(eventToken, (err: any) => {
