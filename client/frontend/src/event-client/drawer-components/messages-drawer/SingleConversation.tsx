1319e130a5dc2d52e3171ff6fd073e6c678fd1fd
Smit Desai <smitdesai422@gmail.com>
2020-07-09 16:56:14 -0700

    Add adaptive timestamp to conversation

--- client/frontend/src/event-client/drawer-components/messages-drawer/SingleConversation.tsx
+++ client/frontend/src/event-client/drawer-components/messages-drawer/SingleConversation.tsx
@@ -57,7 +57,38 @@ const useStyles = makeStyles((_: any) => ({
     margin: 1.5,
     marginTop: 27,
   },
+  timeStamp: {
+    textAlign: "right",
+    marginTop: -20,
+    marginRight: -120,
+    color: "gray",
+  },
 }));
+
+const monthDict = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+const weekDict = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 
 type Props = {
   conversation: any;
@@ -81,6 +112,35 @@ function SingleConversation(props: Props) {
     return joinedString;
   }
 
+  function formatTime(date: Date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    let minutesString = minutes < 10 ? "0" + minutes : minutes;
+    var strTime = hours + ":" + minutesString + " " + ampm;
+
+    return strTime;
+  }
+
+  function getConversationTime(date: Date) {
+    const now = new Date();
+    const messageYear = date.getFullYear();
+    const messageMonth = date.getMonth();
+    const messageDate = date.getDate();
+    const currDate = now.getDate();
+    if (messageYear !== now.getFullYear()) return messageYear;
+    else if (messageMonth !== now.getMonth()) return monthDict[messageMonth];
+    else if (currDate - messageDate >= 7)
+      return `${messageMonth}/${messageDate}`;
+    else if (currDate - messageDate > 1) return weekDict[date.getDay()];
+    else if (messageDate !== currDate) return "Yesterday";
+    else return formatTime(date);
+  }
+
+  const date = new Date(props.conversation.lastMessage.timeCreated);
+  const time = getConversationTime(date);
   return (
     <div
       className={styles.wrapper}
@@ -104,6 +164,9 @@ function SingleConversation(props: Props) {
         <div className={styles.left}>
           <RSText size={12} className={styles.message}>
             {props.conversation.lastMessage.content}
+          </RSText>
+          <RSText size={10} className={styles.timeStamp}>
+            {time}
           </RSText>
         </div>
       </div>
