7f4825b8ad13f523e8f775cf25bb9f91acd1cd4d
Ashwin Mahesh <ashwin@Ashwins-MBP-2.attlocal.net>
2020-07-16 23:02:20 -0700

    Finished communities page

--- client/frontend/src/main-platform/your-communities/components/YourCommunitiesBody.tsx
+++ client/frontend/src/main-platform/your-communities/components/YourCommunitiesBody.tsx
@@ -2,10 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
 import { colors } from '../../../theme/Colors';
-import RSText from '../../../base-components/RSText';
-
 import { WelcomeMessage } from '../../reusable-components';
-import MakePostContainer from './MakePostContainer';
 
 const HEADER_HEIGHT = 60;
 
@@ -15,18 +12,27 @@ const useStyles = makeStyles((_: any) => ({
     background: colors.primaryText,
     overflow: 'scroll',
   },
+  body: {},
+  searchBar: {
+    flex: 1,
+    marginRight: 10,
+  },
+  searchBarContainer: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+    marginLeft: 20,
+    marginRight: 20,
+  },
 }));
 
 type Props = {};
 
-function HomepageBody(props: Props) {
+function YourCommunitiesBody(props: Props) {
   const styles = useStyles();
-
   const [loading, setLoading] = useState(true);
   const [height, setHeight] = useState(window.innerHeight - HEADER_HEIGHT);
-  //TODO - Use default state false for this once connected to server, and set to true if its their first visit
   const [showWelcomeModal, setShowWelcomeModal] = useState(true);
-  const [postValue, setPostValue] = useState('');
+  const [autocompleteResults, setAutocompleteResults] = useState([]);
 
   useEffect(() => {
     window.addEventListener('resize', handleResize);
@@ -47,42 +53,18 @@ function HomepageBody(props: Props) {
     setShowWelcomeModal(false);
   }
 
-  function handlePostValueChange(event: any) {
-    setPostValue(event.target.value);
-  }
-
-  function handleImageUpload() {
-    console.log('Uploading image');
-  }
-
-  function handleSubmitPost() {
-    console.log('Submitting post');
-  }
-
-  function handleDiscoverClick() {
-    window.location.href = `${window.location.protocol}//${window.location.host}/discover`;
-  }
-
   return (
     <div className={styles.wrapper} style={{ height: height }}>
       {showWelcomeModal && (
         <WelcomeMessage
-          title="Welcome to RootShare!"
-          message="Every success story is rooted in the support from a community. Join your
-        communities or discover new ones today."
+          title="Communities"
+          message="All of the communities that you belong to will be displayed on this page. We believe in the power of community and want to make it as easy to access as possible"
           onClose={closeWelcomeMessage}
-          buttonText="Discover"
-          buttonAction={handleDiscoverClick}
         />
       )}
-      <MakePostContainer
-        postValue={postValue}
-        onChange={handlePostValueChange}
-        onPost={handleSubmitPost}
-        onUploadImage={handleImageUpload}
-      />
+      <div className={styles.body}></div>
     </div>
   );
 }
 
-export default HomepageBody;
+export default YourCommunitiesBody;
