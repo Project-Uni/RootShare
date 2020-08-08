7a0095cd89da85f2c299c35ae28162c98ba58c3c
Ashwin Mahesh <mahesh2@purdue.edu>
2020-08-08 10:49:49 -0700

    Added profile drawer implementation

--- client/frontend/src/event-client/drawer-components/ProfileDrawer.tsx
+++ client/frontend/src/event-client/drawer-components/ProfileDrawer.tsx
@@ -1,8 +1,13 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+import ProfilePicture from '../../base-components/ProfilePicture';
 
 const useStyles = makeStyles((_: any) => ({
   wrapper: {},
+  profilePicture: {
+    marginTop: 20,
+  },
 }));
 
 type Props = {};
@@ -11,7 +16,13 @@ function ProfileDrawer(props: Props) {
   const styles = useStyles();
   return (
     <div className={styles.wrapper}>
-      <p>I am the Profile Drawer</p>
+      <ProfilePicture
+        className={styles.profilePicture}
+        editable
+        height={150}
+        width={150}
+        borderRadius={150}
+      />
     </div>
   );
 }
