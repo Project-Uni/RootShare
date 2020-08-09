7b2bdc8b5e5813ce6850178ff2ba74d451471cad
jacksonmccluskey <jackson.mccluskey@gmail.com>
2020-08-09 06:44:37 +0200

    1v1

---
+++ client/frontend/src/landing-new/landing-components/LandingHead.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar } from '@material-ui/core';
+import RSText from '../../base-components/RSText';
+import { Link } from 'react-router-dom';
+
+import RootShareLogoWhite from '../../images/RootShareLogoWhite.png';
+
+import { colors } from '../../theme/Colors';
+
+const useStyles = makeStyles((_: any) => ({
+  header: {
+    marginBottom: '5px',
+    background: colors.second,
+    width: '100vw',
+    minWidth: 450,
+  },
+  headerTitle: {
+    flexGrow: 1,
+    textAlign: 'left',
+  },
+  loginButton: {
+    flexGrow: 1,
+    textAlign: 'right',
+  },
+  headerLogo: {
+    color: 'white',
+    height: '38px',
+    width: '190px',
+  },
+}));
+
+type Props = {};
+
+function LandingHead(props: Props) {
+  const styles = useStyles();
+  return (
+    <AppBar position="static" className={styles.header}>
+      <Toolbar>
+        <div className={styles.headerTitle}>
+          <a href="/">
+            <img
+              src={RootShareLogoWhite}
+              alt="RootShare"
+              className={styles.headerLogo}
+            />
+          </a>
+        </div>
+        <div className={styles.loginButton}>
+          <Link to="../home" style={{ textDecoration: 'none' }}>
+            <RSText type="head" size={15} color={colors.primaryText}>
+              Login
+            </RSText>
+          </Link>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default LandingHead;
