0a083acd2c4d5b80cebea5c27f6eddbab9519366
jacksonmccluskey <jackson.mccluskey@gmail.com>
2020-08-09 23:12:44 +0200

    Complete Profile UI Done + Static + Update + Variables + No Server Save

--- client/frontend/src/event-client/drawer-components/ProfileDrawer.tsx
+++ client/frontend/src/event-client/drawer-components/ProfileDrawer.tsx
@@ -7,7 +7,8 @@ import { updateUser } from '../../redux/actions/user';
 import { updateAccessToken, updateRefreshToken } from '../../redux/actions/token';
 import { makeRequest } from '../../helpers/makeRequest';
 import { colors } from '../../theme/Colors';
-import { TextField } from '@material-ui/core';
+import { Select, MenuItem } from '@material-ui/core';
+import UserInfoTextField from './UserInfoTextField';
 
 import ProfilePicture from '../../base-components/ProfilePicture';
 
@@ -21,8 +22,15 @@ const useStyles = makeStyles((_: any) => ({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  names: {
+    display: 'flex',
+    justifyContent: 'flex-start',
+  },
   nameWrapper: {
     marginTop: 20,
+  },
+  lastName: {
+    marginLeft: 20,
   },
   name: {
     marginTop: 10,
@@ -31,15 +39,19 @@ const useStyles = makeStyles((_: any) => ({
     justifyContent: 'center',
   },
   static: {
-    marginTop: 10,
+    marginTop: 20,
     marginLeft: 20,
   },
   staticIndividual: {
     marginTop: 20,
   },
   update: {
-    marginTop: 10,
+    marginTop: 30,
     marginLeft: 20,
+  },
+  updateIndividual: {
+    marginTop: 20,
+    color: colors.primaryText,
   },
   button: {
     marginRight: 20,
@@ -48,6 +60,7 @@ const useStyles = makeStyles((_: any) => ({
     background: colors.bright,
   },
   buttonWrapper: {
+    marginBottom: 10,
     textAlign: 'right',
   },
 }));
@@ -61,8 +74,215 @@ function ProfileDrawer(props: Props) {
   const [currentPicture, setCurrentPicture] = useState<string>();
   const [imageLoaded, setImagedLoaded] = useState(false);
   const [edit, setEdit] = useState(false);
+  const [original, setOriginal] = useState(true);
+
+  // Original User Information
+  // Constant Variables For Primary Info
+
+  const [originalFirstName, setOriginalFirstName] = useState('');
+  const [originalLastName, setOriginalLastName] = useState('');
+  const [originalMajor, setOriginalMajor] = useState('');
+  const [originalGraduationYear, setOriginalGraduationYear] = useState('');
+  const [originalCurrentEmployer, setOriginalCurrentEmployer] = useState('');
+  const [originalCurrentRole, setOriginalCurrentRole] = useState('');
+  const [originalCollege, setOriginalCollege] = useState('');
+  const [originalInterests, setOriginalInterests] = useState('');
+  const [originalOrganizations, setOriginalOrganizations] = useState('');
+  const [originalGraduateDegree, setOriginalGraduateDegree] = useState('');
+  const [originalPhoneNumber, setOriginalPhoneNumber] = useState('');
+  const [originalDiscoveryMethod, setOriginalDiscoveryMethod] = useState('');
+
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [major, setMajor] = useState('');
+  const [graduationYear, setGraduationYear] = useState('');
+  const [currentEmployer, setCurrentEmployer] = useState('');
+  const [currentRole, setCurrentRole] = useState('');
+  const [college, setCollege] = useState('');
+  const [interests, setInterests] = useState('');
+  const [organizations, setOrganizations] = useState('');
+  const [graduateDegree, setGraduateDegree] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [discoveryMethod, setDiscoveryMethod] = useState('');
+
+  useEffect(() => {
+    setOriginalUserInfo();
+  }, []);
+
+  // Set All Initial User Info
+  // TODO: Map From Server From Specific User Instead Of Ashwin's Info
+
+  function setOriginalUserInfo() {
+    setOriginalFirstName(props.user.firstName);
+    setOriginalLastName(props.user.lastName);
+    setOriginalMajor('Computer Science');
+    setOriginalGraduationYear('2020');
+    setOriginalCurrentEmployer('AutoZone');
+    setOriginalCurrentRole('Senior Lead Software Engineer');
+    setOriginalCollege('Purdue University');
+    setOriginalInterests('Baddies, Hoes, Thots');
+    setOriginalOrganizations('PIKE, Volleyball, APhi Sexers');
+    setOriginalGraduateDegree('No');
+    setOriginalPhoneNumber('4086449017');
+    setOriginalDiscoveryMethod('Creator of Platform');
+
+    setFirstName(props.user.firstName);
+    setLastName(props.user.lastName);
+    setMajor('Computer Science');
+    setGraduationYear('2020');
+    setCurrentEmployer('AutoZone');
+    setCurrentRole('Senior Lead Software Engineer');
+    setCollege('Purdue University');
+    setInterests('Baddies, Hoes, Thots');
+    setOrganizations('PIKE, Volleyball, APhi Sexers');
+    setGraduateDegree('No');
+    setPhoneNumber('4086449017');
+    setDiscoveryMethod('Creator of Platform');
+  }
+
+  // Changed User Information
+  // Constant Variables For Changed Info
+
+  function updateNewUserInfoToServer() {
+    // TODO: Map Out All The Variables To Server To Store Changed User Info
+
+    console.log(firstName);
+    console.log(lastName);
+    console.log(major);
+    console.log(graduationYear);
+    console.log(currentEmployer);
+    console.log(currentRole);
+    console.log(college);
+    console.log(interests);
+    console.log(organizations);
+    console.log(graduateDegree);
+    console.log(phoneNumber);
+    console.log(discoveryMethod);
+
+    if (firstName !== originalFirstName) {
+      setOriginalFirstName(firstName);
+    }
+
+    if (lastName !== originalLastName) {
+      setOriginalLastName(firstName);
+    }
+
+    if (major !== originalMajor) {
+      setOriginalMajor(major);
+    }
+
+    if (graduationYear !== originalGraduationYear) {
+      setOriginalGraduationYear(graduationYear);
+    }
+
+    if (currentEmployer !== originalCurrentEmployer) {
+      setOriginalCurrentEmployer(currentEmployer);
+    }
+
+    if (currentRole !== originalCurrentRole) {
+      setOriginalCurrentRole(currentRole);
+    }
+
+    if (college !== originalCollege) {
+      setOriginalCollege(college);
+    }
+
+    if (interests !== originalInterests) {
+      setOriginalInterests(interests);
+    }
+
+    if (organizations !== originalOrganizations) {
+      setOriginalOrganizations(organizations);
+    }
+
+    if (graduateDegree !== originalGraduateDegree) {
+      setOriginalGraduateDegree(graduateDegree);
+    }
+
+    if (phoneNumber !== originalPhoneNumber) {
+      setOriginalPhoneNumber(phoneNumber);
+    }
+
+    if (discoveryMethod !== originalDiscoveryMethod) {
+      setOriginalDiscoveryMethod(discoveryMethod);
+    }
+  }
+
+  // Handle User Info Change Functions
+  // TODO: Find What To Pass In As "value"
+
+  function handleFirstNameChange(event: any) {
+    console.log('Handling first name change...');
+    setFirstName(event.target.value);
+  }
+
+  function handleLastNameChange(event: any) {
+    console.log('Handling last name change...');
+    setLastName(event.target.value);
+  }
+
+  function handleMajorChange(event: any) {
+    console.log('Handling major change...');
+    setMajor(event.target.value);
+  }
+
+  function handleGraduationYearChange(event: any) {
+    console.log('Handling grad year change...');
+    setGraduationYear(event.target.value);
+  }
+
+  function handleCurrentEmployerChange(event: any) {
+    console.log('Handling current employer change...');
+    setCurrentEmployer(event.target.value);
+  }
+
+  function handleCurrentRoleChange(event: any) {
+    console.log('Handling current role change...');
+    setCurrentRole(event.target.value);
+  }
+
+  function handleCollegeChange(event: any) {
+    console.log('Handling college change...');
+    setCollege(event.target.value);
+  }
+
+  function handleInterestsChange(event: any) {
+    console.log('Handling interests change...');
+    setInterests(event.target.value);
+  }
+
+  function handleOrganizationsChange(event: any) {
+    console.log('Handling organizations change...');
+    setOrganizations(event.target.value);
+  }
+
+  function handleGraduateDegreeChange(event: any) {
+    console.log('Handling graduate degree change...');
+    setGraduateDegree(event.target.value);
+  }
+
+  function handlePhoneNumberChange(event: any) {
+    console.log('Handling phone number change...');
+    setPhoneNumber(event.target.value);
+  }
+
+  function handleDiscoveryMethodChange(event: any) {
+    console.log('Handling discovery method change...');
+    setDiscoveryMethod(event.target.value);
+  }
+
+  // End Handle Change Functions For User Info
 
   function editOnClick() {
+    setEdit(!edit);
+  }
+
+  function saveOnClick() {
+    setEdit(!edit);
+    updateNewUserInfoToServer();
+  }
+
+  function cancelOnClick() {
     setEdit(!edit);
   }
 
@@ -91,11 +311,19 @@ function ProfileDrawer(props: Props) {
       <div className={styles.buttonWrapper}>
         <Button
           onClick={() => {
-            editOnClick();
+            saveOnClick();
           }}
           className={styles.button}
         >
           SAVE
+        </Button>
+        <Button
+          onClick={() => {
+            cancelOnClick();
+          }}
+          className={styles.button}
+        >
+          CANCEL
         </Button>
       </div>
     );
@@ -126,7 +354,7 @@ function ProfileDrawer(props: Props) {
           className={styles.name}
           bold
         >
-          First Last
+          {originalFirstName} {originalLastName}
         </RSText>
         <RSText
           type="body"
@@ -134,7 +362,7 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.name}
         >
-          email@email.com
+          {props.user.email}
         </RSText>
       </div>
     );
@@ -142,9 +370,73 @@ function ProfileDrawer(props: Props) {
 
   function returnUpdate() {
     return (
-      <div>
-        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
-        {returnSaveButton()};
+      <div className={styles.update}>
+        <div className={styles.names}>
+          <UserInfoTextField
+            label="First Name"
+            value={firstName}
+            onChange={handleFirstNameChange}
+            width={170}
+          />
+          <UserInfoTextField
+            label="Last Name"
+            value={lastName}
+            onChange={handleLastNameChange}
+            width={170}
+            className={styles.lastName}
+          />
+        </div>
+        <UserInfoTextField
+          label="Major"
+          value={major}
+          onChange={handleMajorChange}
+        />
+        <UserInfoTextField
+          label="Graduation Year"
+          value={graduationYear}
+          onChange={handleGraduationYearChange}
+        />
+        <UserInfoTextField
+          label="Current Employer"
+          value={currentEmployer}
+          onChange={handleCurrentEmployerChange}
+        />
+        <UserInfoTextField
+          label="Current Role"
+          value={currentRole}
+          onChange={handleCurrentRoleChange}
+        />
+        <UserInfoTextField
+          label="College"
+          value={college}
+          onChange={handleCollegeChange}
+        />
+        <UserInfoTextField
+          label="Interests"
+          value={interests}
+          onChange={handleInterestsChange}
+        />
+        <UserInfoTextField
+          label="Organizations"
+          value={organizations}
+          onChange={handleOrganizationsChange}
+        />
+        <UserInfoTextField
+          label="Graduate Degree"
+          value={graduateDegree}
+          onChange={handleGraduateDegreeChange}
+        />
+        <UserInfoTextField
+          label="Phone Number"
+          value={phoneNumber}
+          onChange={handlePhoneNumberChange}
+        />
+        <UserInfoTextField
+          label="Discovery Method"
+          value={discoveryMethod}
+          onChange={handleDiscoveryMethodChange}
+        />
+        {returnSaveButton()}
       </div>
     );
   }
@@ -157,7 +449,7 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Major:
+          Major: {originalMajor}
         </RSText>
         <RSText
           type="body"
@@ -165,7 +457,7 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Graduation Year:
+          Graduation Year: {originalGraduationYear}
         </RSText>
         <RSText
           type="body"
@@ -173,7 +465,7 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Current Employer:
+          Current Employer: {originalCurrentEmployer}
         </RSText>
         <RSText
           type="body"
@@ -181,7 +473,7 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Current Role:
+          Current Role: {originalCurrentRole}
         </RSText>
         <RSText
           type="body"
@@ -189,7 +481,7 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          College:
+          College: {originalCollege}
         </RSText>
         <RSText
           type="body"
@@ -197,7 +489,7 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Interests:
+          Interests: {originalInterests}
         </RSText>
         <RSText
           type="body"
@@ -205,7 +497,8 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Organizations:
+          Organizations: {originalOrganizations}
+          {organizations && !original}
         </RSText>
         <RSText
           type="body"
@@ -213,7 +506,8 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Graduate Degree:
+          Graduate Degree: {originalGraduateDegree}
+          {graduateDegree && !original}
         </RSText>
         <RSText
           type="body"
@@ -221,7 +515,7 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Phone Number:
+          Phone Number: {originalPhoneNumber}
         </RSText>
         <RSText
           type="body"
@@ -229,15 +523,15 @@ function ProfileDrawer(props: Props) {
           color={colors.primaryText}
           className={styles.staticIndividual}
         >
-          Discovery Method:
+          Discovery Method: {originalDiscoveryMethod}
         </RSText>
-        {returnEditButton()};
+        {returnEditButton()}
       </div>
     );
   }
-  return (
-    <div className={styles.wrapper}>
-      {imageLoaded && (
+  function returnProfilePicture() {
+    return (
+      <div>
         <ProfilePicture
           className={styles.profilePicture}
           editable
@@ -247,8 +541,15 @@ function ProfileDrawer(props: Props) {
           currentPicture={currentPicture}
           updateCurrentPicture={updateCurrentPicture}
         />
-      )}
-      {returnNameAndEmail()};{!edit && returnStatic()};{edit && returnUpdate()};
+      </div>
+    );
+  }
+  return (
+    <div className={styles.wrapper}>
+      {imageLoaded && returnProfilePicture()}
+      {returnNameAndEmail()}
+      {!edit && returnStatic()}
+      {edit && returnUpdate()}
     </div>
   );
 }
