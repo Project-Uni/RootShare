102b3fc6f0c837f06ae81678fe78b34cb0cba0e8
jacksonmccluskey <jackson.mccluskey@gmail.com>
2020-08-09 09:17:12 +0200

    Static Labels Profile + Edit Button Switch

--- client/frontend/src/event-client/drawer-components/ProfileDrawer.tsx
+++ client/frontend/src/event-client/drawer-components/ProfileDrawer.tsx
@@ -1,17 +1,54 @@
 import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-
+import { Button } from '@material-ui/core';
+import RSText from '../../base-components/RSText';
 import { connect } from 'react-redux';
 import { updateUser } from '../../redux/actions/user';
 import { updateAccessToken, updateRefreshToken } from '../../redux/actions/token';
 import { makeRequest } from '../../helpers/makeRequest';
+import { colors } from '../../theme/Colors';
+import { TextField } from '@material-ui/core';
 
 import ProfilePicture from '../../base-components/ProfilePicture';
 
 const useStyles = makeStyles((_: any) => ({
-  wrapper: {},
+  wrapper: {
+    width: '400px',
+  },
   profilePicture: {
     marginTop: 20,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nameWrapper: {
+    marginTop: 20,
+  },
+  name: {
+    marginTop: 10,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  static: {
+    marginTop: 10,
+    marginLeft: 20,
+  },
+  staticIndividual: {
+    marginTop: 20,
+  },
+  update: {
+    marginTop: 10,
+    marginLeft: 20,
+  },
+  button: {
+    marginRight: 20,
+    marginTop: 20,
+    color: colors.primaryText,
+    background: colors.bright,
+  },
+  buttonWrapper: {
+    textAlign: 'right',
   },
 }));
 
@@ -23,6 +60,11 @@ function ProfileDrawer(props: Props) {
   const styles = useStyles();
   const [currentPicture, setCurrentPicture] = useState<string>();
   const [imageLoaded, setImagedLoaded] = useState(false);
+  const [edit, setEdit] = useState(false);
+
+  function editOnClick() {
+    setEdit(!edit);
+  }
 
   useEffect(() => {
     getCurrentProfilePicture();
@@ -43,6 +85,156 @@ function ProfileDrawer(props: Props) {
   function updateCurrentPicture(imageData: string) {
     setCurrentPicture(imageData);
   }
+
+  function returnSaveButton() {
+    return (
+      <div className={styles.buttonWrapper}>
+        <Button
+          onClick={() => {
+            editOnClick();
+          }}
+          className={styles.button}
+        >
+          SAVE
+        </Button>
+      </div>
+    );
+  }
+
+  function returnEditButton() {
+    return (
+      <div className={styles.buttonWrapper}>
+        <Button
+          onClick={() => {
+            editOnClick();
+          }}
+          className={styles.button}
+        >
+          EDIT
+        </Button>
+      </div>
+    );
+  }
+
+  function returnNameAndEmail() {
+    return (
+      <div className={styles.nameWrapper}>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.name}
+          bold
+        >
+          First Last
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.name}
+        >
+          email@email.com
+        </RSText>
+      </div>
+    );
+  }
+
+  function returnUpdate() {
+    return (
+      <div>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        {returnSaveButton()};
+      </div>
+    );
+  }
+  function returnStatic() {
+    return (
+      <div className={styles.static}>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Major:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Graduation Year:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Current Employer:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Current Role:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          College:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Interests:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Organizations:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Graduate Degree:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Phone Number:
+        </RSText>
+        <RSText
+          type="body"
+          size={12}
+          color={colors.primaryText}
+          className={styles.staticIndividual}
+        >
+          Discovery Method:
+        </RSText>
+        {returnEditButton()};
+      </div>
+    );
+  }
   return (
     <div className={styles.wrapper}>
       {imageLoaded && (
@@ -56,6 +248,7 @@ function ProfileDrawer(props: Props) {
           updateCurrentPicture={updateCurrentPicture}
         />
       )}
+      {returnNameAndEmail()};{!edit && returnStatic()};{edit && returnUpdate()};
     </div>
   );
 }
