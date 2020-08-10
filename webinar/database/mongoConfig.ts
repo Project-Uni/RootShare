1c55f9b05860d94f7f2b37b5d7988d303dc9a125
Ashwin Mahesh <mahesh2@purdue.edu>
2020-08-09 22:47:30 -0700

    Cleanup

--- webinar/database/mongoConfig.ts
+++ webinar/database/mongoConfig.ts
@@ -4,19 +4,17 @@ import log from '../helpers/logger';
 
 const uri = `mongodb+srv://dbUser1:${DB_KEY}@clusteru-xuq4v.mongodb.net/test?retryWrites=true&w=majority`;
 
-module.exports = {
-  connectDB: function (callback) {
-    mongoose
-      .connect(uri, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-        useCreateIndex: true,
-      })
-      .then(() => {
-        log('info', 'Connected to Database');
-      })
-      .catch((err) => {
-        log('error', err.message);
-      });
-  },
-};
+export function connectDB(callback) {
+  mongoose
+    .connect(uri, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+    .then(() => {
+      log('info', 'Connected to Database');
+    })
+    .catch((err) => {
+      log('error', err.message);
+    });
+}
