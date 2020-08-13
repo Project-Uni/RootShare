a04b54095a18812d4c7442500de0221bd67aeda2
Ashwin Mahesh <mahesh2@purdue.edu>
2020-08-12 23:24:18 -0700

    Updated routes to navigate to event page on login

--- client/frontend/src/landing-page/landing-components/LandingBody.tsx
+++ client/frontend/src/landing-page/landing-components/LandingBody.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import RSText from '../../base-components/RSText';
 import { colors } from '../../theme/Colors';
