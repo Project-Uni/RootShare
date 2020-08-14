cf96ccfb5a1753a93a56df8877167c153ef78fb5
Ashwin Mahesh <mahesh2@purdue.edu>
2020-08-13 23:35:49 -0700

    Adds is host prop to event messages

--- client/frontend/src/event-client/event-messages/EventMessageContainer.tsx
+++ client/frontend/src/event-client/event-messages/EventMessageContainer.tsx
@@ -37,6 +37,7 @@ type Props = {
   refreshToken: string;
   newMessage: MessageType;
   messageSocket: SocketIOClient.Socket;
+  isHost?: boolean;
 };
 
 function EventMessageContainer(props: Props) {
