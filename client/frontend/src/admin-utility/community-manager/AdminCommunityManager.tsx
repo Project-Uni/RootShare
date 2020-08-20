d2c5e3dfcabe89f641752a6e284521f21c1083b3
Ashwin Mahesh <mahesh2@purdue.edu>
2020-08-20 19:41:05 -0400

    Added auth check and header to communities page

--- client/frontend/src/admin-utility/community-manager/AdminCommunityManager.tsx
+++ client/frontend/src/admin-utility/community-manager/AdminCommunityManager.tsx
@@ -1,19 +1,132 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+
+import { CircularProgress } from '@material-ui/core';
+
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import { updateUser } from '../../redux/actions/user';
+import { updateAccessToken, updateRefreshToken } from '../../redux/actions/token';
+import { makeRequest } from '../../helpers/functions';
+import { colors } from '../../theme/Colors';
+
+import RSText from '../../base-components/RSText';
+import EventClientHeader from '../../event-client/EventClientHeader';
+
+const MIN_ACCESS_LEVEL = 6;
 
 const useStyles = makeStyles((_: any) => ({
   wrapper: {},
+  loadingIndicator: {
+    color: colors.primary,
+    marginTop: 100,
+  },
 }));
 
-type Props = {};
+//TODO - Update user type
+type Props = {
+  user: { [key: string]: any };
+  accessToken: string;
+  refreshToken: string;
+  updateUser: (userInfo: { [key: string]: any }) => void;
+  updateAccessToken: (accessToken: string) => void;
+  updateRefreshToken: (refreshToken: string) => void;
+};
 
 function AdminCommunityManager(props: Props) {
   const styles = useStyles();
+
+  const [loading, setLoading] = useState(true);
+  const [loginRedirect, setLoginRedirect] = useState(false);
+  const [showInvalid, setShowInvalid] = useState(false);
+
+  useEffect(() => {
+    checkAuth().then(async (authorized) => {
+      if (authorized) {
+        //TODO - fetch communities
+      }
+      setLoading(false);
+    });
+  }, []);
+
+  async function checkAuth() {
+    const { data } = await makeRequest(
+      'GET',
+      '/user/getCurrent',
+      {},
+      true,
+      props.accessToken,
+      props.refreshToken
+    );
+    if (data['success'] !== 1) {
+      setLoginRedirect(true);
+      return false;
+    } else {
+      props.updateUser({ ...data['content'] });
+      if (data['content']['privilegeLevel'] < MIN_ACCESS_LEVEL) {
+        setShowInvalid(true);
+        return false;
+      }
+    }
+    return true;
+  }
+
+  function renderInvalid() {
+    return (
+      <RSText type="subhead" size={32} bold>
+        Permission not granted to view page
+      </RSText>
+    );
+  }
+
+  function renderPageContent() {
+    return (
+      <div>
+        <p>I am the correct page</p>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.wrapper}>
-      <p>I am a template</p>
+      {loginRedirect && <Redirect to="/login?redirect=/admin/community" />}
+      <EventClientHeader showNavigationWidth={9999} />
+      {loading ? (
+        <CircularProgress
+          className={styles.loadingIndicator}
+          size={100}
+          color="primary"
+        />
+      ) : showInvalid ? (
+        renderInvalid()
+      ) : (
+        renderPageContent()
+      )}
     </div>
   );
 }
 
-export default AdminCommunityManager;
+const mapStateToProps = (state: { [key: string]: any }) => {
+  return {
+    user: state.user,
+    accessToken: state.accessToken,
+    refreshToken: state.refreshToken,
+  };
+};
+
+const mapDispatchToProps = (dispatch: any) => {
+  return {
+    updateUser: (userInfo: { [key: string]: any }) => {
+      dispatch(updateUser(userInfo));
+    },
+    updateAccessToken: (accessToken: string) => {
+      dispatch(updateAccessToken(accessToken));
+    },
+    updateRefreshToken: (refreshToken: string) => {
+      dispatch(updateRefreshToken(refreshToken));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(AdminCommunityManager);
