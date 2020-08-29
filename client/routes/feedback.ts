54075d795d1e4b44e86414566e5ab260d02aca51
Ashwin Mahesh <mahesh2@purdue.edu>
2020-08-28 20:12:39 -0400

    Created route functionality

--- client/routes/feedback.ts
+++ client/routes/feedback.ts
@@ -1,5 +1,6 @@
 import { isAuthenticatedWithJWT } from '../passport/middleware/isAuthenticated';
 import log from '../helpers/logger';
+import sendPacket from '../helpers/sendPacket';
 
 import { request } from '@octokit/request';
 
@@ -29,4 +30,48 @@ export default function feedbackRoutes(app: Express) {
     log('github', `Created new reported bug: title`);
     return res.json({ result });
   });
+
+  app.post('/api/feedback/bug', isAuthenticatedWithJWT, async (req, res) => {
+    //TODO - Decide how to handle images, its not a feature provided by the github api
+
+    const { title, category, message } = req.body;
+    if (!title || !category || !message)
+      return res.json(
+        sendPacket(-1, 'title, category, or message missing from request body')
+      );
+
+    const body = generateGithubIssueContent(category, message, req.user);
+    const result = await requestWithAuth(
+      `POST /repos/${PROJECT_OWNER}/${REPO}/issues`,
+      {
+        title: 'Test New Issue',
+        body: 'I am testing to see if I can programmatically create a new issue',
+        labels: ['Bug'],
+        assignees: ['caitecap'],
+      }
+    );
+    log('github', `Created new reported bug: ${title}`);
+    return res.json(sendPacket(1, 'Successfully reported your bug'));
+  });
+}
+
+function generateGithubIssueContent(
+  category: string,
+  message: string,
+  user: { [key: string]: any }
+) {
+  const output = `
+  **${category}**\n\n
+  
+  **Description**\n
+  ${message}\n\n
+
+  **Reporter**\n
+  ${user.firstName} ${user.lastName}\n
+  **Contact:**\n
+  ${user.email}\n
+  ${user.phoneNumber || ''}
+  `;
+
+  return output;
 }
