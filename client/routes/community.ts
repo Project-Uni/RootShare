906f3263b6cf1484b9dc12450add66cce12d2c3c
Ashwin Mahesh <mahesh2@purdue.edu>
2020-09-01 01:39:54 -0400

    Added todo for update point in route from get to post

--- client/routes/community.ts
+++ client/routes/community.ts
@@ -105,6 +105,7 @@ export default function communityRoutes(app) {
     }
   );
 
+  //TODO - Change this route to a POST route since it is updating data not just retrieving it
   app.get(
     '/api/community/:communityID/:newStatus',
     isAuthenticatedWithJWT,
