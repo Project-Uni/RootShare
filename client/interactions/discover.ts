52ca9a42ea3fea417533e333451aad775ab38a4a
Ashwin Mahesh <mahesh2@purdue.edu>
2020-09-07 19:16:48 -0400

    Community DB call for population done

--- client/interactions/discover.ts
+++ client/interactions/discover.ts
@@ -4,8 +4,47 @@ import { User, Community } from '../models';
 
 const NUM_RETRIEVED = 20;
 
-export function populateDiscoverForUser(userID: string) {
+export async function populateDiscoverForUser(userID: string) {
   const numUsers = Math.floor(Math.random() * 10) + 5;
   const numCommunities = NUM_RETRIEVED - numUsers;
-  return sendPacket(1, 'Test worked');
+
+  // const communitiesPromise = User.aggregate({ $match: { $not: {$in: {joinedCommunities: }} } });
+  try {
+    const communityPromise = Community.aggregate([
+      {
+        $match: {
+          $and: [
+            { members: { $not: { $eq: userID } } },
+            { pendingMembers: { $not: { $eq: userID } } },
+          ],
+        },
+      },
+      { $sample: { size: numCommunities } },
+    ])
+      .select([
+        'name',
+        'type',
+        'description',
+        'private',
+        'members',
+        'profilePicture',
+      ])
+      .exec();
+
+    return Promise.all([communityPromise])
+      .then(([communities]) => {
+        return sendPacket(
+          1,
+          'Successfully retrieved users and communities for population',
+          { communities }
+        );
+      })
+      .catch((err) => {
+        log('error', err);
+        return sendPacket(-1, err);
+      });
+  } catch (err) {
+    log('error', err);
+    return sendPacket(-1, err);
+  }
 }
