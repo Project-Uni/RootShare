0265ba98dfa915bf3daf387c212ea3369bedf456
Ashwin Mahesh <mahesh2@purdue.edu>
2020-09-09 19:13:43 -0400

    Remove contact link

--- client/frontend/src/landing-page/landing-components/LandingFooter.tsx
+++ client/frontend/src/landing-page/landing-components/LandingFooter.tsx
@@ -49,14 +49,11 @@ function LandingFooter(props: Props) {
       <Toolbar>
         <div className={styles.footerContainer}>
           <ButtonGroup variant="text" aria-label="text primary button group">
-          <a href='mailto:dev@rootshare.io' style={{ textDecoration: 'none' }}>
-          <Button>
-            <RSText type="body" size={12} color={colors.primaryText}>
-              CONTACT
-            </RSText>
-          </Button>
-          </a>
-            {/*
+            {/* <Button>
+              <RSText type="body" size={12} color={colors.primaryText}>
+                ABOUT
+              </RSText>
+            </Button>
             <Button>
               <RSText type="body" size={12} color={colors.primaryText}>
                 HELP
