1a58a4ffa95059a0c034d91b5f9f4694ddaf1041
Ashwin Mahesh <mahesh2@purdue.edu>
2020-09-16 20:49:31 -0400

    Created routes for posting and retrieving internal alumni

--- client/routes/posts.ts
+++ client/routes/posts.ts
@@ -2,11 +2,15 @@ import { sendPacket } from '../helpers/functions';
 import { isAuthenticatedWithJWT } from '../passport/middleware/isAuthenticated';
 
 import {
+  //General Posting
   createBroadcastUserPost,
   getGeneralFeed,
   getPostsByUser,
+  //New Community Internal
   createInternalCurrentMemberCommunityPost,
+  createInternalAlumniPost,
   getInternalCurrentMemberPosts,
+  getInternalAlumniPosts,
 } from '../interactions/posts';
 
 export default function postsRoutes(app) {
@@ -55,6 +59,27 @@ export default function postsRoutes(app) {
     }
   );
 
+  app.post(
+    '/api/posts/community/:communityID/internal/alumni',
+    isAuthenticatedWithJWT,
+    async (req, res) => {
+      const { communityID } = req.params;
+      const { _id: userID, accountType } = req.user;
+      const { message } = req.body;
+
+      if (!message)
+        return res.json(sendPacket(-1, 'message is missing from request body'));
+
+      const packet = await createInternalAlumniPost(
+        communityID,
+        userID,
+        accountType,
+        message
+      );
+      return res.json(packet);
+    }
+  );
+
   app.get(
     '/api/posts/community/:communityID/internal/current',
     isAuthenticatedWithJWT,
@@ -67,6 +92,18 @@ export default function postsRoutes(app) {
         userID,
         accountType
       );
+      return res.json(packet);
+    }
+  );
+
+  app.get(
+    '/api/posts/community/:communityID/internal/alumni',
+    isAuthenticatedWithJWT,
+    async (req, res) => {
+      const { communityID } = req.params;
+      const { _id: userID, accountType } = req.user;
+
+      const packet = await getInternalAlumniPosts(communityID, userID, accountType);
       return res.json(packet);
     }
   );
