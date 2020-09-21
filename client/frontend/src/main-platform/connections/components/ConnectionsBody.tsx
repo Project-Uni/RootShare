8374638684ff996862a596268412955884bac2bc
zdutta <zidaandutta123@gmail.com>
2020-09-20 23:32:31 -0400

    changing from full name to first name

--- client/frontend/src/main-platform/connections/components/ConnectionsBody.tsx
+++ client/frontend/src/main-platform/connections/components/ConnectionsBody.tsx
@@ -105,7 +105,7 @@ function ConnectionsBody(props: Props) {
       props.refreshToken
     );
     if (data.success === 1) {
-      setUsername(`${data.content.user?.firstName} ${data.content.user?.lastName}`);
+      setUsername(`${data.content.user?.firstName}`);
     }
   }
 
