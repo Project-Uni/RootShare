a975b3f614ac40bf1b2b323b4ac2757e7785b337
Ashwin Mahesh <mahesh2@purdue.edu>
2020-11-11 21:03:55 -0500

    Created theme file

---
+++ client/frontend/src/theme/Theme.js
@@ -0,0 +1,28 @@
+const theme = {
+  default: {
+    white: '#F8F8FF',
+    primary: '#1D2445',
+    secondary: '',
+    background: 'rgb(227, 227, 227)',
+    primaryText: 'black',
+    secondaryText: '#6D738E',
+    error: '#440C16',
+    success: '#4BB543',
+    highlight: '#6699FF',
+    highlightText: '#F8F8FF',
+  },
+  dark: {
+    white: '',
+    primary: '',
+    secondary: '',
+    background: '',
+    primaryText: '',
+    secondaryText: '',
+    error: '',
+    success: '',
+    highlight: '',
+    highlightText: '',
+  },
+};
+
+export default theme;
