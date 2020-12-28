9fd7a747e1985ec97dece69e137312626ea621bd
Ashwin Mahesh <mahesh2@purdue.edu>
2020-12-28 00:11:07 -0800

    Added twilio webhooks

--- client/routes/webhooks.ts
+++ client/routes/webhooks.ts
@@ -1,8 +1,8 @@
 import { Express } from 'express';
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
+const { MessagingResponse, VoiceResponse } = require('twilio').twiml;
 
 export default function webhooks(app: Express) {
-  app.post('/api/webhooks/twilio/sms', async (req, res) => {
+  app.post('/api/webhooks/twilio/sms', (req, res) => {
     const twilio_response = new MessagingResponse();
     twilio_response.message(
       'This is an automated message from RootShare, the host of Meet The Greeks.'
@@ -13,5 +13,18 @@ export default function webhooks(app: Express) {
 
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(twilio_response.toString());
+  });
+
+  app.post('/api/webhooks/twilio/voice', (req, res) => {
+    const twilio_response = new VoiceResponse();
+    twilio_response.say(
+      { voice: 'alice' },
+      `This is an automated system from RootShare, the host of Meet The Greeks. 
+      To contact the sender of the message, please reach out to someone in the fraternity directly. 
+      Thank you, and boiler up!`
+    );
+
+    res.type('text/xml');
+    res.send(twilio_response.toString());
   });
 }
