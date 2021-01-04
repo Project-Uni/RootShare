a6ee2deea6dc7b521d88c96efcb608c9d7a5a8a9
Ashwin Mahesh <mahesh2@purdue.edu>
2021-01-03 18:38:17 -0800

    Moved props

--- client/frontend/src/main-platform/reusable-components/components/UserSearch.tsx
+++ client/frontend/src/main-platform/reusable-components/components/UserSearch.tsx
@@ -35,7 +35,7 @@ type ServiceResponse = {
 type Props<T extends SearchOption> = {
   className: string;
   options?: T[];
-  fetchDataURL?: string; // URL for fetching data
+  fetchDataURL?: string;
   name: string;
   label: string;
   helperText?: string;
@@ -47,15 +47,27 @@ type Props<T extends SearchOption> = {
 function UserSearch<T extends SearchOption = SearchOption>(props: Props<T>) {
   const styles = useStyles();
 
-  const [options, setOptions] = useState(props.options || []);
+  const {
+    className,
+    options: optionsProps,
+    fetchDataURL,
+    name,
+    label,
+    helperText,
+    onAutocomplete: onAutocompleteProps,
+    error,
+    mapData,
+  } = props;
+
+  const [options, setOptions] = useState(optionsProps || []);
   const [searchValue, setSearchValue] = useState('');
 
   const onAutocomplete = (_: any, newValue: T | null) => {
-    if (newValue) props.onAutocomplete?.(newValue);
+    if (newValue) onAutocompleteProps?.(newValue);
     setSearchValue('');
   };
 
-  const defaultMap = useCallback(
+  const defaultMapData = useCallback(
     (users: User[]): T[] =>
       users.map((user) => ({
         _id: user._id,
@@ -67,43 +79,37 @@ function UserSearch<T extends SearchOption = SearchOption>(props: Props<T>) {
   );
 
   const fetchData = async () => {
-    if (props.fetchDataURL) {
-      const { data } = await makeRequest<ServiceResponse>('GET', props.fetchDataURL);
+    if (fetchDataURL) {
+      const { data } = await makeRequest<ServiceResponse>('GET', fetchDataURL);
       if (data.success === 1) {
         setOptions(
-          props.mapData
-            ? props.mapData(data.content.users)
-            : defaultMap(data.content.users)
+          mapData ? mapData(data.content.users) : defaultMapData(data.content.users)
         );
       }
     }
   };
 
   useEffect(() => {
-    if (props.fetchDataURL) fetchData();
-  }, [searchValue, props.fetchDataURL]);
+    if (fetchDataURL) fetchData();
+  }, [searchValue, fetchDataURL]);
 
   return (
     <Autocomplete
-      className={props.className}
+      className={className}
       options={options}
       inputValue={searchValue}
       getOptionLabel={(option) => option.value}
       onChange={onAutocomplete}
-      key={`autocompleted_${props.label}`}
+      key={`autocompleted_${label}`}
       renderInput={(params) => (
         <TextField
           {...params}
-          label={props.label}
+          label={label}
           variant="outlined"
           onChange={(e) => setSearchValue(e.target.value)}
           fullWidth
-          helperText={
-            Boolean(props.error) && props.error !== ''
-              ? props.error
-              : props.helperText
-          }
-          error={Boolean(props.error) && props.error !== ''}
+          helperText={Boolean(error) && error !== '' ? error : helperText}
+          error={Boolean(error) && error !== ''}
         />
       )}
       renderOption={(option) => (
