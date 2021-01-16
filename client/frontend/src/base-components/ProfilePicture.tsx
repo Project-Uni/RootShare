2a9498d6b5cd3ccb290007e52174f8fe1765ab3c
Smit Desai <smitdesai422@gmail.com>
2021-01-16 15:25:50 -0600

    Fix Bug

--- client/frontend/src/base-components/ProfilePicture.tsx
+++ client/frontend/src/base-components/ProfilePicture.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Carousel, { Modal, ModalGateway } from 'react-images';
 import { FaCamera } from 'react-icons/fa';
