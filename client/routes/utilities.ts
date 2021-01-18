81bcc988b3972809510a533ec0f51145cb0805cc
Ashwin Mahesh <mahesh2@purdue.edu>
2021-01-17 17:57:15 -0800

    Added utility functions post MTG

--- client/routes/utilities.ts
+++ client/routes/utilities.ts
@@ -1,5 +1,5 @@
 import { isAuthenticatedWithJWT } from '../passport/middleware/isAuthenticated';
-import { User } from '../models';
+import { MeetTheGreekInterest, User, Webinar } from '../models';
 
 const aws = require('aws-sdk');
 aws.config.loadFromPath('../keys/aws_key.json');
@@ -18,6 +18,23 @@ module.exports = (app) => {
     getUserData((packet) => {
       res.json(packet);
     });
+  });
+
+  app.get('/api/interested', async (req, res) => {
+    const data = await getUniqueInterested();
+    return res.json(
+      sendPacket(1, 'Retrieved Data', {
+        users: data,
+        count: Object.keys(data).length,
+      })
+    );
+  });
+
+  app.get('/api/viewers', async (req, res) => {
+    const data = await getTotalMTGViewers();
+    return res.json(
+      sendPacket(1, 'Retrieved Viewers:', { viewers: data, length: data.length })
+    );
   });
 
   // app.get('/api/twilio', async (req: Request, res: Response) => {
@@ -96,4 +113,64 @@ function sendEmail(email, subject, message) {
     .catch((err) => {
       log('error', `${email}: ${err}`);
     });
+}
+
+async function getUniqueInterested() {
+  const data: any[] = await MeetTheGreekInterest.find({}, 'answers user')
+    .populate({ path: 'user', select: 'firstName lastName email phoneNumber' })
+    .exec();
+
+  const reducedData = {};
+  data.forEach((currentInterest) => {
+    if (!(currentInterest.user._id in reducedData)) {
+      reducedData[
+        currentInterest.user._id
+      ] = `${currentInterest.user.firstName} ${currentInterest.user.lastName}`;
+    }
+  });
+
+  return reducedData;
+}
+
+async function getTotalMTGViewers() {
+  const events = await Webinar.find({ isMTG: true, isDev: false }, 'attendees_V2')
+    .populate({ path: 'attendees_V2', select: 'firstName lastName email _id' })
+    .lean()
+    .exec();
+
+  const output = {};
+  const chain = events
+    .map((event) => event.attendees_V2)
+    .forEach((event) => {
+      if (event) {
+        event.forEach((attendee) => {
+          if (attendee._id && !(attendee._id in output)) {
+            output[attendee._id] = {
+              firstName: attendee.firstName,
+              lastName: attendee.lastName,
+            };
+          }
+        });
+      }
+    });
+
+  // const first = events.map((event) => event.attendees_V2);
+  // const output = {};
+  // first.forEach((event) => {
+  //   if (event) {
+  //     event.forEach((attendee) => {
+  //       if (attendee._id && !(attendee._id in output)) {
+  //         output[attendee._id] = {
+  //           firstName: attendee.firstName,
+  //           lastName: attendee.lastName,
+  //         };
+  //       }
+  //     });
+  //   }
+  // });
+
+  const filteredOutput = Object.keys(output).map(
+    (key) => `${output[key].firstName} ${output[key].lastName}`
+  );
+  return filteredOutput;
 }
