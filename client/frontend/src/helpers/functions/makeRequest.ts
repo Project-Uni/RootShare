324eaca81fffbfec73bc0d99f1d6f9c00e111c69
Ashwin Mahesh <mahesh2@purdue.edu>
2021-02-21 18:27:17 -0800

    New login working

--- client/frontend/src/helpers/functions/makeRequest.ts
+++ client/frontend/src/helpers/functions/makeRequest.ts
@@ -1,34 +1,35 @@
-import axios, { AxiosResponse } from 'axios';
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios';
 import { getStore } from '../../redux/store/persistedStore';
 
-type Config = {
-  headers: {
-    Authorization: string;
-  };
+type HttpResponse<T> = {
+  success: -1 | 0 | 1;
+  message: string;
+  content: T;
 };
 
-export function makeRequest<T = { [key: string]: any }>(
+export async function makeRequest<T = { [key: string]: any }>(
   method: 'GET' | 'POST' | 'PUT' | 'DELETE',
   url: string,
-  data: { [key: string]: any } = {},
+  data?: { [key: string]: any },
   ...rest: any
-): Promise<AxiosResponse<{ success: -1 | 0 | 1; message: string; content: T }>> {
+): Promise<AxiosResponse<HttpResponse<T>>> {
   const { accessToken, refreshToken } = getStore().getState();
 
-  const config: Config = {
+  const config: AxiosRequestConfig = {
     headers: {
-      Authorization: `Bearer ${accessToken}`,
+      Authorization:
+        accessToken && refreshToken ? `Bearer ${accessToken}` : undefined,
     },
+    method,
+    url,
+    data,
   };
 
-  switch (method) {
-    case 'GET':
-      return axios.get(url, accessToken && refreshToken ? config : {});
-    case 'POST':
-      return axios.post(url, data, accessToken && refreshToken ? config : {});
-    case 'PUT':
-      return axios.put(url, data, accessToken && refreshToken ? config : {});
-    case 'DELETE':
-      return axios.delete(url, accessToken && refreshToken ? config : {});
+  try {
+    let response: AxiosResponse<HttpResponse<T>> = await axios(config);
+    return response;
+  } catch (err) {
+    let error = err as AxiosError;
+    return error.response as AxiosResponse<HttpResponse<T>>;
   }
 }
--- client/routes/newAuth.ts
+++ client/routes/newAuth.ts
@@ -1,9 +1,15 @@
 import { Express } from 'express';
+import { send } from 'process';
 import sendPacket from '../../webinar/helpers/sendPacket';
 import { isAuthenticatedWithJWT } from '../../webinar/middleware/isAuthenticated';
-import { generateJWT, hashPassword, getQueryParams } from '../helpers/functions';
+import {
+  generateJWT,
+  hashPassword,
+  getQueryParams,
+  comparePasswords,
+} from '../helpers/functions';
 import { User } from '../models';
-import {} from '../models/users';
+import { getUsersByIDs } from '../models/users';
 
 export const authRoutes = (app: Express) => {
   app.get('/api/v2/auth/validate', async (req, res) => {
@@ -15,6 +21,26 @@ export const authRoutes = (app: Express) => {
     if (!query) return res.status(400).json(sendPacket(-1, 'Missing query params'));
 
     const { email, password, phoneNumber } = query;
+    // if (!isValidEmail(email))
+    //   return res.status(400).json(sendPacket(-1, 'Email address not valid'));
+
+    // if (!isValidPassword(password))
+    //   return res.status(400).json(sendPacket(-1, 'Invalid Password'));
+
+    // if (!isValidPhoneNumber(phoneNumber))
+    //   return res.status(500).json(sendPacket(-1, 'Invalid phone number'));
+    try {
+      const userExists = await User.exists({
+        email: { $regex: email, $options: 'i' },
+      });
+      if (userExists)
+        res
+          .status(400)
+          .json(sendPacket(0, 'Account with this email already exists'));
+      else res.status(200).json(sendPacket(1, 'New account information is valid'));
+    } catch (err) {
+      res.status(500).json(sendPacket(-1, 'There was an error validating the user'));
+    }
   });
 
   app.post('/api/v2/auth/register', async (req, res) => {
@@ -64,45 +90,38 @@ export const authRoutes = (app: Express) => {
     else if (accountType === 'faculty' && !jobTitle)
       return res.status(400).json(sendPacket(-1, 'Missing body parameters'));
 
-    // if (!isValidEmail(email))
-    //   return res.status(400).json(sendPacket(-1, 'Email address not valid'));
-
-    // if (!isValidPassword(password))
-    //   return res.status(400).json(sendPacket(-1, 'Invalid Password'));
-
     try {
-      const userExists = await User.exists({
-        email: { $regex: email, $options: 'i' },
-      });
-      if (userExists)
-        return res
-          .status(400)
-          .json(sendPacket(0, 'Account with this email already exists'));
-
       const newUser = await new User({
         email: email.toLowerCase(),
         phoneNumber,
         hashedPassword: hashPassword(password),
+        firstName,
+        lastName,
+        accountType,
+        major,
+        position: jobTitle,
+        work: company,
+        graduationYear,
+        // state
       }).save();
 
-      //Generate JWT
       const { accessToken, refreshToken } = generateJWT({
         email: email.toLowerCase(),
         _id: newUser._id,
         privilegeLevel: 1,
-        firstName: '',
-        lastName: '',
-        accountType: '',
+        firstName: firstName,
+        lastName: lastName,
+        accountType: accountType,
       });
 
       return res.status(200).json(
         sendPacket(1, 'Successfully registered user', {
           user: {
-            firstName: '',
-            lastName: '',
-            email: email.toLowerCase(),
-            accountType: '',
-            privilegeLevel: 1,
+            firstName: newUser.firstName,
+            lastName: newUser.lastName,
+            email: newUser.email,
+            accountType: newUser.accountType,
+            privilegeLevel: newUser.privilegeLevel,
           },
           accessToken,
           refreshToken,
@@ -115,13 +134,52 @@ export const authRoutes = (app: Express) => {
     }
   });
 
-  app.post('/api/v2/auth/login', async (req, res) => {});
+  app.post('/api/v2/auth/login', async (req, res) => {
+    const { email, password }: { email: string; password: string } = req.body;
+    try {
+      const validation_user = await User.findOne(
+        {
+          email: { $regex: email, $options: 'i' },
+        },
+        ['hashedPassword']
+      )
+        .lean()
+        .exec();
 
-  app.post(
-    '/api/v2/auth/registration/complete',
-    isAuthenticatedWithJWT,
-    async (req, res) => {
-      const { accountType, firstName, lastName };
+      if (!validation_user)
+        return res.status(400).json(sendPacket(0, 'No user exists with this email'));
+
+      if (!comparePasswords(password, validation_user.hashedPassword))
+        return res.status(400).json(sendPacket(0, 'Invalid password'));
+
+      const [user] = await getUsersByIDs([validation_user._id], {
+        fields: [
+          'firstName',
+          'lastName',
+          'accountType',
+          'privilegeLevel',
+          'profilePicture',
+        ],
+        options: {
+          includeDefaultFields: false,
+          getProfilePicture: true,
+          limit: 1,
+        },
+      });
+
+      console.log('User:', user);
+
+      const { accessToken, refreshToken } = generateJWT(user);
+
+      return res.status(200).json(
+        sendPacket(1, 'Successfully logged in', {
+          user,
+          accessToken,
+          refreshToken,
+        })
+      );
+    } catch (err) {
+      return res.status(400).json(sendPacket(0, 'Failed to find user'));
     }
-  );
+  });
 };
