b8db89e3309a6a7aead93b2319d2fb094f2b9cb3
Alex Thrift <akeizerthrift@gmail.com>
2021-02-23 13:33:01 -0500

    internal login

--- client/frontend/src/App.tsx
+++ client/frontend/src/App.tsx
@@ -13,8 +13,8 @@ import Login from './login/Login'; //OLD COMPONENT
 import ResetPassword from './login/ResetPassword';
 import SocketManager from './main-platform/SocketManager';
 
-// import LandingPage from './landing-page/LandingPage'; //OLD LANDING PAGE
-import LandingPage from './landing-page/redesign/LandingPage'; //NEW LANDING PAGE
+import LandingPage from './landing-page/LandingPage'; //OLD LANDING PAGE
+// import LandingPage from './landing-page/redesign/LandingPage'; //NEW LANDING PAGE
 
 import {
   // MeetTheGreeks,
@@ -30,8 +30,8 @@ import { AdminRoutes } from './routes';
 import AuthenticatedPage from './main-platform/AuthenticatedPage/AuthenticatedPage';
 import { SnackbarNotification } from './main-platform/reusable-components';
 import FollowSidebar from './main-platform/community/components/Sidebar/FollowSidebar';
-import AccountTypeSelect from './landing-page/redesign/AccountTypeSelect'; //NEW ACCOUNT TYPE SELECT
-import Community from './main-platform/community/redesign/Community'; //NEW COMMUNITY
+// import AccountTypeSelect from './landing-page/redesign/AccountTypeSelect'; //NEW ACCOUNT TYPE SELECT
+// import Community from './main-platform/community/redesign/Community'; //NEW COMMUNITY
 
 const analyticsTrackingID = 'UA-169916177-1';
 ReactGA.initialize(analyticsTrackingID);
@@ -51,7 +51,7 @@ const App = () => {
       <Router history={history}>
         <div className="wrapper">
           <Switch>
-            <Route exact path="/" render={() => <LandingPage mode="register" />} />
+            {/* <Route exact path="/" render={() => <LandingPage mode="register" />} />
 
             <Route exact path="/account/select" component={AccountTypeSelect} />
             <Route
@@ -59,8 +59,8 @@ const App = () => {
               path="/account/initialize"
               render={() => <LandingPage mode="additional" />}
             />
-            <Route exact path="/login" render={() => <LandingPage mode="login" />} />
-            {/* <Route exact path="/" component={LandingPage} />
+            <Route exact path="/login" render={() => <LandingPage mode="login" />} /> */}
+            <Route exact path="/" component={LandingPage} />
             <Route
               exact
               path="/register/external"
@@ -71,7 +71,7 @@ const App = () => {
               path="/register/initialize"
               component={HypeAdditionalInfo}
             />
-            <Route exact path="/login" component={Login} /> */}
+            <Route exact path="/login" component={Login} />
 
             <Route
               exact
@@ -108,9 +108,9 @@ const App = () => {
               path="/community/:communityID"
               render={(props) => (
                 <AuthenticatedPage
-                  component={<Community />} //NEW COMMUNITY UI
-                  // component={<CommunityBody {...props} />} //OLD COMMUNITY
-                  // rightElement={<FollowSidebar />} //OLD COMMUNITY
+                  // component={<Community />} //NEW COMMUNITY UI
+                  component={<CommunityBody {...props} />} //OLD COMMUNITY
+                  rightElement={<FollowSidebar />} //OLD COMMUNITY
                 />
               )}
             />
--- client/frontend/src/landing-page/redesign/LandingPage.tsx
+++ client/frontend/src/landing-page/redesign/LandingPage.tsx
@@ -4,13 +4,17 @@ import RootShareLogo from '../../images/RootShareLogoFull.png';
 import LandingImg from '../../images/landingBullets.png';
 import { RSText } from '../../base-components';
 import { TextField, Link } from '@material-ui/core';
-import { RSButton } from '../..//main-platform/reusable-components';
+import { RSButton } from '../../main-platform/reusable-components';
+import { makeRequest } from '../../helpers/functions';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateUser } from '../../redux/actions/user';
+import { updateAccessToken, updateRefreshToken } from '../../redux/actions/token';
+
 import Theme from '../../theme/Theme';
 import GoogleButton from '../../hype-page/hype-registration/GoogleButton';
 import LinkedInButton from '../../hype-page/hype-registration/LinkedInButton';
 import { useHistory } from 'react-router-dom';
 import queryString from 'query-string';
-import { useSelector } from 'react-redux';
 import { RootshareReduxState } from '../../redux/store/stateManagement';
 import { trace } from 'console';
 
@@ -105,20 +109,21 @@ type Props = {
 const LandingPage = (props: Props) => {
   const styles = useStyles();
   const history = useHistory();
+  const dispatch = useDispatch();
 
   const accessToken = useSelector((state: RootshareReduxState) => state.accessToken);
 
   const { mode } = props;
   const [isMobile, setIsMobile] = useState(window.innerWidth < MIN_WIDTH);
 
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
   const [forgotPassword, setForgotPassword] = useState(false);
 
   // const values = queryString.parse(props.location.search);
-   const redirectUrl = '/home';
+  const redirectUrl = '/home';
   // const accessTokenParam = values.accessToken as string;
   // const refreshTokenParam = values.refreshToken as string;
 
@@ -129,6 +134,7 @@ const LandingPage = (props: Props) => {
 
   const checkAuth = useCallback(() => {
     if (Boolean(accessToken)) history.push('/home');
+    setLoading(false);
   }, [accessToken]);
 
   useEffect(() => {
@@ -139,6 +145,57 @@ const LandingPage = (props: Props) => {
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, [handleResize]);
+
+  const textFieldProps: any = {
+    variant: 'standard',
+    error,
+    onKeyDown: handleEnterCheck,
+    className: styles.textBox,
+  };
+
+  function handleEnterCheck(event: any) {
+    if (event.key === 'Enter') handleLogin();
+  }
+
+  async function handleLogin() {
+    setLoading(true);
+    const { data } = await makeRequest('POST', '/api/v2/auth/login', {
+      email: email,
+      password: password,
+    });
+    if (data['success'] === 1) {
+      setError(false);
+      const {
+        firstName,
+        lastName,
+        _id,
+        email,
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken,
+        privilegeLevel,
+        accountType,
+        profilePicture,
+      } = data['content'];
+      dispatch(
+        updateUser({
+          firstName,
+          lastName,
+          _id,
+          email,
+          privilegeLevel,
+          accountType,
+          profilePicture,
+          profilePictureLastUpdated: profilePicture ? Date.now() : undefined,
+        })
+      );
+      dispatch(updateAccessToken(newAccessToken));
+      dispatch(updateRefreshToken(newRefreshToken));
+      history.push(redirectUrl);
+    } else {
+      setError(true);
+    }
+    setLoading(false);
+  }
 
   //TODO - Update with components
   const getLeftComponent = useCallback(() => {
@@ -166,11 +223,64 @@ const LandingPage = (props: Props) => {
     }
   }, [mode]);
 
-  //TODO - Update with components
+  //Moved RightComponent below to prevent Textfeild re-render
   const getRightComponent = useCallback(() => {
     switch (mode) {
       case 'register':
-        return (<div className={styles.rightMiddleContent}>
+        return (
+          <p>Additional Info Form and Buttons</p>
+      )
+      case 'login':
+        return (
+          <p>Additional Info Form and Buttons</p>
+        )
+      case 'additional':
+        return <p>Additional Info Form and Buttons</p>;
+    }
+  }, [mode]);
+
+  return (
+    <div
+      className={styles.wrapper}
+      style={{
+        flexDirection: isMobile ? 'column' : 'row',
+        justifyContent: 'flex-start',
+        display: 'flex',
+      }}
+    >
+      <div
+        className={styles.left}
+        style={{
+          width: isMobile ? '100%' : '40%',
+        }}
+      >
+        {!isMobile && (
+          <div className={styles.leftMiddleContent}>{getLeftComponent()}</div>
+        )}
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: isMobile ? 'center' : 'flex-start',
+            justifyContent: isMobile ? 'center' : undefined,
+            marginLeft: 50,
+            marginBottom: 50,
+          }}
+        >
+          {/* TODO - Fix styling to match wireframe */}
+          {isMobile && (
+            <div className={styles.leftMiddleContent}>{getLeftComponent()}</div>
+          )}
+
+          <img src={RootShareLogo} className={styles.logo} />
+          <RSText color={Theme.white} size={20}>
+            Lets Grow Together
+          </RSText>
+        </div>
+      </div>
+      <div className={styles.right}>
+        {mode === 'register' && ( 
+          <div className={styles.rightMiddleContent}>
           <table>
             <tr>
             <TextField
@@ -267,6 +377,67 @@ const LandingPage = (props: Props) => {
               </Link>
             </div>
           </tr>
+          </table>
+        </div>
+        )}
+        {mode === 'login' && (
+          <div className={styles.rightMiddleContent}>
+          <table>
+          <tr>
+            <TextField
+              {...textFieldProps}
+              label="E-MAIL"
+              autoComplete="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              helperText={error ? 'Invalid login credentials' : ''}
+            />
+          </tr>
+          <tr>
+            <TextField
+              {...textFieldProps}
+              label="PASSWORD"
+              autoComplete="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+            />
+          </tr>
+          <tr>
+            <div className={styles.externalWrapper}>
+              <GoogleButton messageType={'login'} width={500} redirect={redirectUrl} />
+            </div>
+          </tr>
+          <tr>
+            <div className={styles.externalWrapper}>
+              <LinkedInButton
+                messageType={'login'}
+                width={500}
+                redirect={redirectUrl}
+              />
+            </div>
+          </tr>
+          <tr>
+            <div className={styles.buttonContainer}>
+              <RSButton
+                variant="primary"
+                onClick={handleLogin}
+                className={styles.button}
+                disabled={loading}
+              >
+                Login
+              </RSButton>
+              <RSText color={Theme.secondaryText} className={styles.or} size={12}>
+                or
+              </RSText>
+              <Link
+                href={"/"}
+                className={styles.link}
+              >
+                Sign-Up
+              </Link>
+            </div>
+          </tr>
           <tr>
             <div className={styles.right}>
             <Link
@@ -280,143 +451,8 @@ const LandingPage = (props: Props) => {
           </tr>
           </table>
         </div>
-      )
-      case 'login':
-        return (
-          <div className={styles.rightMiddleContent}>
-            <table>
-            <tr>
-              <TextField
-                // {...textFieldProps}
-                label="E-MAIL"
-                autoComplete="email"
-                className={styles.textBox}
-                // onChange={(e) => setEmail(e.target.value)}
-                // value={email}
-                // helperText={error ? 'Invalid login credentials' : ''}
-              />
-            </tr>
-            <tr>
-              <TextField
-                // {...textFieldProps}
-                label="PASSWORD"
-                autoComplete="password"
-                className={styles.textBox}
-                // onChange={(e) => setPassword(e.target.value)}
-                // value={password}
-                // type="password"
-              />
-            </tr>
-            <tr>
-              <div className={styles.externalWrapper}>
-                <GoogleButton messageType={'login'} width={500} redirect={redirectUrl} />
-              </div>
-            </tr>
-            <tr>
-              <div className={styles.externalWrapper}>
-                <LinkedInButton
-                  messageType={'login'}
-                  width={500}
-                  redirect={redirectUrl}
-                />
-              </div>
-            </tr>
-            <tr>
-              <div className={styles.buttonContainer}>
-                <RSText color={Theme.secondaryText} className={styles.or} size={12}>
-                  By signing up I agree to the
-                </RSText>
-                <Link
-                  href={""}
-                  className={styles.link}
-                >
-                  terms and conditions
-                </Link>
-                <RSText color={Theme.secondaryText} size={12}>
-                  .
-                </RSText>
-              </div>
-            </tr>
-            <tr>
-              <div className={styles.buttonContainer}>
-                <RSButton
-                  // variant="contained"
-                  // onClick={handleLogin}
-                  className={styles.button}
-                  // disabled={loading}
-                >
-                  Login
-                </RSButton>
-                <RSText color={Theme.secondaryText} className={styles.or} size={12}>
-                  or
-                </RSText>
-                <Link
-                  href={"/"}
-                  className={styles.link}
-                >
-                  Sign-Up
-                </Link>
-              </div>
-            </tr>
-            <tr>
-              <div className={styles.right}>
-              <Link
-                href={undefined}
-                className={styles.link}
-                onClick={() => setForgotPassword(true)}
-              >
-                Forgot Password?
-              </Link>
-              </div>
-            </tr>
-            </table>
-          </div>
-        )
-      case 'additional':
-        return <p>Additional Info Form and Buttons</p>;
-    }
-  }, [mode]);
-
-  return (
-    <div
-      className={styles.wrapper}
-      style={{
-        flexDirection: isMobile ? 'column' : 'row',
-        justifyContent: 'flex-start',
-        display: 'flex',
-      }}
-    >
-      <div
-        className={styles.left}
-        style={{
-          width: isMobile ? '100%' : '40%',
-        }}
-      >
-        {!isMobile && (
-          <div className={styles.leftMiddleContent}>{getLeftComponent()}</div>
         )}
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: isMobile ? 'center' : 'flex-start',
-            justifyContent: isMobile ? 'center' : undefined,
-            marginLeft: 50,
-            marginBottom: 50,
-          }}
-        >
-          {/* TODO - Fix styling to match wireframe */}
-          {isMobile && (
-            <div className={styles.leftMiddleContent}>{getLeftComponent()}</div>
-          )}
-
-          <img src={RootShareLogo} className={styles.logo} />
-          <RSText color={Theme.white} size={20}>
-            Lets Grow Together
-          </RSText>
-        </div>
       </div>
-      <div className={styles.right}>{getRightComponent()}</div>
     </div>
   );
 };
