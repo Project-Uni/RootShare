db5a91ceb8e81949788a91cecf3cc5a0c09b2260
Smit Desai <smitdesai422@gmail.com>
2021-03-25 18:40:58 -0700

    WIP

--- client/frontend/src/main-platform/community/redesign/CommunityMedia.tsx
+++ client/frontend/src/main-platform/community/redesign/CommunityMedia.tsx
@@ -8,15 +8,31 @@ import { useDispatch, useSelector } from 'react-redux';
 import { dispatchSnackbar } from '../../../redux/actions';
 import { RootshareReduxState } from '../../../redux/store/stateManagement';
 
+import { RSCard } from '../../reusable-components';
+
 import { getCommunityMedia } from '../../../api';
 import { ImageType } from '../../../helpers/types';
 import Theme from '../../../theme/Theme';
 
-const IMAGE_SIZE = 150;
+// const IMAGE_SIZE = 150;
 
 const useStyles = makeStyles((_: any) => ({
   wrapper: {},
+  cardWrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+
+    paddingLeft: 40,
+    paddingRight: 40,
+    paddingTop: 30,
+    paddingBottom: 30,
+    marginBottom: 30,
+    textAlign: 'left',
+  },
   image: {
+    height: '33%',
+    width: '33%',
+    objectFit: 'cover',
     '&:hover': {
       cursor: 'pointer',
     },
@@ -62,14 +78,16 @@ export const CommunityMedia = (props: Props) => {
       {loading ? (
         <CircularProgress size={90} style={{ color: Theme.bright, marginTop: 50 }} />
       ) : (
-        images?.map((image, i) => (
-          <img
-            src={image.fileName}
-            style={{ height: IMAGE_SIZE, width: IMAGE_SIZE, objectFit: 'cover' }}
-            className={styles.image}
-            onClick={() => setImageViewerIndex(i)}
-          />
-        ))
+        <RSCard className={styles.cardWrapper}>
+          {images?.map((image, i) => (
+            <img
+              src={image.fileName}
+              // style={{ height: IMAGE_SIZE, width: IMAGE_SIZE, objectFit: 'cover' }}
+              className={styles.image}
+              onClick={() => setImageViewerIndex(i)}
+            />
+          ))}
+        </RSCard>
       )}
       <ModalGateway>
         {imageViewerIndex >= 0 && (
