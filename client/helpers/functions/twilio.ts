cce735545cca109472d298048f1d67842f65e205
Ashwin Mahesh <mahesh2@purdue.edu>
2021-04-11 20:55:15 -0700

    Cleaned up sms code

--- client/helpers/functions/twilio.ts
+++ client/helpers/functions/twilio.ts
@@ -1,3 +1,5 @@
+import { log } from './logger';
+
 const { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } = require('../../../keys/keys.json');
 
 const twilio_client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
@@ -13,24 +15,51 @@ export async function sendSMS(
     formattedNumbers.push(formatSMSRecipients(recipients[i]));
   }
 
-  const smsPromises = formattedNumbers.map((phoneNumber) => {
-    if (!phoneNumber) return null;
-    return twilio_client.messages.create({
-      to: phoneNumber,
-      from: TWILIO_PHONE_NUMBER,
-      body: message,
-      mediaUrl: imageURL,
-    });
+  const smsPromises = formattedNumbers.map(async (phoneNumber) => {
+    if (!phoneNumber)
+      return { success: true, message: 'No associated or invalid number' };
+    try {
+      await twilio_client.messages.create({
+        to: phoneNumber,
+        from: TWILIO_PHONE_NUMBER,
+        body: message,
+        mediaUrl: imageURL,
+      });
+      return { success: true, message: 'Sent message successfully' };
+    } catch (err) {
+      log('Twilio IO Error', err.message);
+      return { success: false, message: err };
+    }
   });
 
   return Promise.all(smsPromises)
     .then((values) => {
-      console.log('Twilio success values:', values);
+      let someSuccessFlag = false;
+      let hasErr = false;
+
+      values.forEach((v) => {
+        if (v && !someSuccessFlag) someSuccessFlag = true;
+        if (!v && !hasErr) hasErr = true;
+      });
+
+      if (hasErr) {
+        if (someSuccessFlag)
+          return {
+            success: 1,
+            message:
+              'There was an error sending some messages, but most were sent successfully',
+          };
+        return {
+          success: -1,
+          message: 'There was an error sending all messages',
+        };
+      }
+
       return { success: 1, message: 'Successfully sent all messages' };
     })
     .catch((err) => {
-      console.log('Twilio IO Error:', err);
-      return { success: -1, message: err.message };
+      log('unhandled error', err.message);
+      return { success: 0, message: `Unhandled Twilio Error: ${err.message}` };
     });
 }
 
